66827005aa258ca9f822e0a06d0b25f1642a32f7
Xavier <xav29dl@hotmail.com>
2022-11-04 11:45:01 -0400

    First vesrion portfolio

--- wristband-react-app/src/library.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    
-    "FooterContent":"Lorem ipsum dolor sit amet consectetur adipisicing elit.Doloribus quo fuga ducimus iure numquam vel vero ratione, tempore totam quis.Nobis molestias distinctio iure odit quia alias, neque quod eos."
-};
